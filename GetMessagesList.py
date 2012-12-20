8af8ca8df43d965f12c708a5d65259a13666b052
Denis Irz <Denirz@gmail.com>
2012-12-20 09:34:51 +0400

    get messagelist changed

--- GetMessagesList.py
+++ GetMessagesList.py
@@ -3,7 +3,10 @@ Created on Dec 20, 2012
 
 @author: denirz
 '''
-
+def GetMessageList(Jsession,_umscsrf):
+    print "GetMessageListOutput:"
+    pass
 if __name__ == '__main__':
     print "GetMessagesList Module output:"
+    GetMessageList('ldd','123523542342')
     pass
