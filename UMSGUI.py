ceabd8e4e6ef3a4de5ede510b423009f74297d3f
Denis Irz <Denirz@gmail.com>
2013-01-05 10:27:58 +0400

    продолжаем строить модуль

--- UMSGUI.py
+++ UMSGUI.py
@@ -13,26 +13,32 @@ class Application(Frame):
     def __init__(self,master=None):
         Frame.__init__(self,master)
 #        self['height']='2000'
-        self.pack()
+        self.pack(expand=1)
         self.createTopFrame()
         self.createSearchFrame()
         MainFrame=self.createMainFrame()
         self.putChatItem(MainFrame)
-        
+        self.putChatItem(MainFrame,'+79262001223','2012-31-10','ну просто достаточно длинный текст чтобы его   было можно перенести, если понадобится - ибо а вдруг понадобится? ')
+        self.putChatItem(MainFrame,'+79262001223','2012-31-11','и еще один такой- же ну просто достаточно длинный текст чтобы его   было можно перенести, если понадобится - ибо а вдруг понадобится? ')
+        self.putChatItem(MainFrame, '+79262131605', '2013-04-01', 'ты  поганый и вонючий урод,   я тебя неавижу! ')
+        self.putChatItem(MainFrame, '+79262131605', '2013-04-01', 'ты  поганый и вонючий урод,   я тебя неавижу! ')
         self.createWidget()
         pass
     def createTopFrame(self):
         self.topFrame=Frame(self,width='200',height='10') # to enhance later the size of the frame
-        self.topFrame.pack(side='top',fill='x')
+#        self.topFrame['expand']='true'
+        self.topFrame.pack(side='top',expand=1,fill='x')
         self.tempTopLabel=Label(self.topFrame,text='[------------TopFrame]')
         self.tempTopLabel.pack(side='right')
+        self.refreshButton=Button(self.topFrame,text="refresh")
+        self.refreshButton.pack(side='left')
         pass
     def createSearchFrame(self):
         self.searchFrame=Frame(self)
 #        self.searchFrame['width']=self.topFrame['width']
         self.searchFrame['height']='20'
         self.searchFrame['background']='Green'
-        self.searchFrame.pack(side='top',fill='x')
+        self.searchFrame.pack(side='top',fill='x',expand=1)
         #label for the searchFrame: 
         self.tempSearchLabel=Label(self.searchFrame,text='[searchFrame]')
         self.tempSearchLabel.pack(side='left')
@@ -58,6 +64,29 @@ class Application(Frame):
 #        self.tempMainLabel.pack(side='bottom')
 
     def putChatItem(self,frame,source='+79262001222',date='2012-12-31',text='text просто проверка'):
+        
+        boldFont=tkFont.Font(family='Arial',size='10',weight='bold')
+        itemF=Frame(frame)
+        itemF.pack(side='top', fill='x',expand='1')
+        itemFHeader=Frame(itemF)
+        itemFHeader.pack(side='top',fill='x',expand='1')
+        itemFText=Frame(itemF)
+        itemFText.pack(side='top',fill='x',expand='1')
+        
+        self.Number=Label(itemFHeader,text=source)
+        self.Number['font']=boldFont
+        self.Number['justify']='right'
+        self.Number.pack(side='right',fill='none')
+        self.Date=Label(itemFHeader,text=date)
+        self.Date['font']=boldFont
+#        self.Date['underline']='1'
+        self.Date['highlightthickness']='2'
+        self.Date.pack(side='right',fill='none')
+        
+        self.smsText=Label(itemFText,text=text)
+        self.smsText['wraplength']='250'
+        self.smsText['justify']='left'
+        self.smsText.pack(side='bottom',pady='0')
         pass
     def createWidget(self):
         pass 
