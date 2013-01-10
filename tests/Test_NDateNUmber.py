9f22c2ebaf94e39e23ed511547d12ee1d712235d
Denis Irz <Denirz@gmail.com>
2013-01-10 11:09:58 +0400

    unittest reversed to the correct state

--- tests/Test_NDateNUmber.py
+++ tests/Test_NDateNUmber.py
@@ -7,7 +7,7 @@ Created on Jan 6, 2013
 @author: denirz
 '''
 import unittest
-from NDateNumber import NormMSISDN,NormDate
+from UMSClientLogin.NDateNumber import NormMSISDN,NormDate
 
 class Test(unittest.TestCase):
 
@@ -16,7 +16,7 @@ class Test(unittest.TestCase):
         inp='9262001222'
         outp='+79262001222'
         r=NormMSISDN(inp,'+')
-        self.assertEqual(r,outp,'expected:'+outp+' received:'+ r)
+        self.assertEqual(r,outp,'input:' +inp+' expected:'+outp+' received:'+ r)
         inp='+79262001222'
         outp=inp
         r=NormMSISDN(inp,'+')
